ecdd1943e22322550ead51c8c847a7b239ddd43a
LENOVO <manasi.dwivedi09@gmail.com>
2025-04-05 16:27:35 +0530

    05- April 2025

---
+++ src/April2025/Lab001_printf_0304.java
@@ -0,0 +1,11 @@
+package April2025;
+
+public class Lab001_printf_0304 {
+    public static void main(String[] args) {
+        int Int=10;
+        System.out.println(Int);
+    }
+
+
+    
+}
---
+++ src/Operators/Interview_0504.java
@@ -0,0 +1,9 @@
+package Operators;
+
+public class Interview_0504 {
+    public static void main(String[] args) {
+        short c=10;
+        char a= 'A';
+        System.out.println(c+a);
+    }
+}
